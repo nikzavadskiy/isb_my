9208b850611e249a1671efd2e8a6cd9e6325045f
PanDiablo <163389043+nikzavadskiy@users.noreply.github.com>
2025-04-16 10:41:28 +0400

    Update decod.py

--- lab_1/task2/decod.py
+++ lab_1/task2/decod.py
@@ -1,3 +1,17 @@
+def count_frequencies(text):
+    text = text.replace('\n', '')
+
+    total_chars = len(text)
+
+    frequency_counter = Counter(text)
+
+    frequencies = {}
+    for char, count in frequency_counter.items():
+        frequencies[char] = round(count / total_chars, 4)
+
+    return dict(sorted(frequencies.items(), key=lambda x: x[1], reverse=True))
+
+
 def decrypt_text(text, key):
     decrypted_text = []
     for char in text:
